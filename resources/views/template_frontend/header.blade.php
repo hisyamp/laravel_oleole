1e578f925bd042999042e15cc24b2bb789842a3f
hisyamp <hisyampriaji.hp@gmail.com>
2021-07-26 02:54:56 +0700

    deploy22

--- resources/views/template_frontend/header.blade.php
+++ resources/views/template_frontend/header.blade.php
@@ -6,25 +6,25 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>travel</title>
-    <link rel="icon" href="{{asset('tourby/img/favicon.png')}}">
+    <link rel="icon" href="{{asset('public/tourby/img/favicon.png')}}">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{asset('tourby/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('public/tourby/css/bootstrap.min.css')}}">
     <!-- animate CSS -->
-    <link rel="stylesheet" href="{{asset('tourby/css/animate.css')}}">
+    <link rel="stylesheet" href="{{asset('public/tourby/css/animate.css')}}">
     <!-- owl carousel CSS -->
-    <link rel="stylesheet" href="{{asset('tourby/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('public/tourby/css/owl.carousel.min.css')}}">
     <!-- font awesome CSS -->
-    <link rel="stylesheet" href="{{asset('tourby/css/all.css')}}">
+    <link rel="stylesheet" href="{{asset('public/tourby/css/all.css')}}">
     <!-- flaticon CSS -->
-    <link rel="stylesheet" href="{{asset('tourby/css/flaticon.css')}}">
-    <link rel="stylesheet" href="{{asset('tourby/css/themify-icons.css')}}">
-    <link rel="stylesheet" href="{{asset('tourby/css/nice-select.css')}}">
+    <link rel="stylesheet" href="{{asset('public/tourby/css/flaticon.css')}}">
+    <link rel="stylesheet" href="{{asset('public/tourby/css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('public/tourby/css/nice-select.css')}}">
     <!-- font awesome CSS -->
-    <link rel="stylesheet" href="{{asset('tourby/css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{asset('public/tourby/css/magnific-popup.css')}}">
     <!-- swiper CSS -->
-    <link rel="stylesheet" href="{{asset('tourby/css/slick.css')}}">
+    <link rel="stylesheet" href="{{asset('public/tourby/css/slick.css')}}">
     <!-- style CSS -->
-    <link rel="stylesheet" href="{{asset('tourby/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('public/tourby/css/style.css')}}">
     
 </head>
 
